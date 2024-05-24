d833701fbcbfb6e54e4530dc330ffd9849181218
Malgorzata Kolka <117841097+malgorzatakolka@users.noreply.github.com>
2024-05-24 14:28:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,8 @@ session = cnx.session()
 my_dataframe = session.table('smoothies.public.fruit_options') \
             .select(col('FRUIT_NAME'))
 
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: ',
@@ -23,6 +25,7 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
         fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
