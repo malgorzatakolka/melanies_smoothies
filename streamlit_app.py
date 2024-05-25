d5d31e984bb3d0f432100aae1df51cbaebdf5343
Malgorzata Kolka <117841097+malgorzatakolka@users.noreply.github.com>
2024-05-25 06:23:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.write(
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table('smoothies.public.fruit_options') \
-            .select(col('FRUIT_NAME', 'SEARCH_ON'))
+            .select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
 pd_df = my_dataframe.to_pandas()
 
